a3e8011b7b608382dd850f562100e9410611d48e
Johnny Jiang <u363084>
2015-11-02 17:21:23 +1100

    [nj] initial commit.

---
+++ Prototype-Skeleton/Prototype-SkeletonTests/Prototype_SkeletonTests.swift
@@ -0,0 +1,36 @@
+//
+//  Prototype_SkeletonTests.swift
+//  Prototype-SkeletonTests
+//
+//  Created by Johnny Jiang on 2/11/2015.
+//  Copyright © 2015 Azolla. All rights reserved.
+//
+
+import XCTest
+@testable import Prototype_Skeleton
+
+class Prototype_SkeletonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
