e1779a5f795668d479b6fc13767a0f09c80e051f
nanjiang <johnny.nan.jiang@gmail.com>
2015-11-04 14:32:17 +1100

    [nj] added raw code

--- Prototype-Skeleton/Prototype-Skeleton/ViewController.swift
+++ Prototype-Skeleton/Prototype-Skeleton/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Optional: Place the button in the center of your view.
+        let loginButton = FBSDKLoginButton();
+        loginButton.center = self.view.center;
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"];
+        loginButton.delegate = self;
+        self.view.addSubview(loginButton);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // >>> Facebook Delegate Methods
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result:FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
+    // <<< Facebook Delegate Methods
 }
 
